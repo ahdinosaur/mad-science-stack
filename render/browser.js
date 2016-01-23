77ee6aba5af7eec385e367ff953f24012bebf998
Michael Williams <michael.williams@enspiral.com>
2016-01-23 15:55:19 +1300

    hot-reload vdux in browser (but overridden by live-reload on purpose)

--- render/browser.js
+++ render/browser.js
@@ -1,13 +1,20 @@
 const vdux = require('vdux/dom').default
 const h = require('vdux/element').default
 
-const reducer = require('app/reducer')
-const middleware = require('app/middleware')
-
-vdux({
-  reducer,
-  middleware,
+const { replace } = vdux({
+  reducer: require('app/reducer'),
+  middleware: require('app/middleware'),
   initialState: window.__state,
   app: (state) => h(require('app'), state),
   node: document.querySelector('main')
 })
+
+if (module.hot) {
+  module.hot.accept(
+    ['app', 'app/reducer'],
+    () => replace(
+      (state) => h(require('app'), state),
+      require('app/reducer')
+    )
+  )
+}
